dba287835ca37fdabe35d257d9de99ca88ab5c85
Manankiyo <100239153+Manankiyo@users.noreply.github.com>
2022-12-07 20:31:33 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,7 @@
+<script>
+                var myInput = document.getElementById("psw"); 
+                var letter = document.getElementById("letter");
+                var capital = document.getElementById("capital");
+                var number = document.getElementById("number");
+                var length = document.getElementById("length");
+                </script>
